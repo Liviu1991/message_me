5d5cb54e8b0a40687c08311d89e1429a8085c3d5
Liviu Cauia <liviu.cauia@gmail.com>
2020-02-10 19:01:29 +0200

    display messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Liviu", password: "password")
+User.create(username: "Andrei", password: "password")
+User.create(username: "Pavel", password: "password")
+User.create(username: "Sergiu", password: "password")
+User.create(username: "Vasea", password: "password")
+User.create(username: "Iulius", password: "password")
